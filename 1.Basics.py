db3ef275f4a4e8d08968147ac38ecbee7253a500
codepiece20 <codepiece20@gmail.com>
2024-07-29 21:40:32 +0500

    Basics

---
+++ 1.Basics.py
@@ -0,0 +1,18 @@
+from math import *
+# Basics
+
+
+# 1. Variables
+
+name = "Walter White"
+age = 55
+
+print(f"His Name is {name} and he is {age}!")
+
+# 2. String and Integer Data Type
+
+print(f"One \nPiece \n{1000 + 100} Episodes")
+
+# 3. Input
+names = input("Enter Your Name: ")
+print(f"Hello {names}")
